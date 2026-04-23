adc256e3cee3dd98751bc9e4a32bcf4004e19d8a
cyptonik <enixydex15@gmail.com>
2026-04-23 07:58:08 +0300

    moved all errors to a separate class

---
+++ src/app/src/main/java/org/weather/app/ErrorMessage.java
@@ -0,0 +1,9 @@
+package org.weather.app;
+
+public class ErrorMessage {
+    public static final String USER_NOT_FOUND = "User with this login was not found";
+    public static final String USER_EXISTS = "User with this login already exists";
+
+    public static final String INVALID_PASSWORD = "Invalid password";
+    public static final String INVALID_PARAMS = "Invalid user or password";
+}
