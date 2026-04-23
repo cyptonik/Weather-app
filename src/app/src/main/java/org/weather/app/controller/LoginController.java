ec0d4a2bd44e8668a5664c3b9354b71439d79c85
cyptonik <enixydex15@gmail.com>
2026-04-23 07:56:54 +0300

    created login and registration controllers

--- src/app/src/main/java/org/weather/app/controller/LoginController.java
+++ src/app/src/main/java/org/weather/app/controller/LoginController.java
@@ -1,18 +1,66 @@
 package org.weather.app.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.weather.app.ErrorMessage;
+import org.weather.app.model.User;
+import org.weather.app.model.UserSession;
+import org.weather.app.repository.SessionRepository;
+import org.weather.app.repository.UserRepository;
+import org.weather.app.service.SessionService;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Controller
 public class LoginController {
+    private final UserRepository userRepository;
+    private final SessionRepository sessionRepository;
+    private final SessionService sessionService;
+
+    public LoginController(UserRepository userRepository, SessionRepository sessionRepository, SessionService sessionService) {
+        this.userRepository = userRepository;
+        this.sessionRepository = sessionRepository;
+        this.sessionService = sessionService;
+    }
+
     @GetMapping("/login")
     public String get() {
         return "login";
     }
 
     @PostMapping("/login")
-    public String post() {
-        return "login";
+    public String post(RedirectAttributes redirectAttributes, HttpServletRequest request, HttpServletResponse response) {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        if (login.isBlank() || password.isBlank()) {
+            redirectAttributes.addFlashAttribute("errorMessage", ErrorMessage.INVALID_PARAMS);
+            return "redirect:/login";
+        }
+
+        User foundUser = userRepository.findByLogin(login).orElse(null);
+
+        if (foundUser == null) {
+            redirectAttributes.addFlashAttribute("errorMessage", ErrorMessage.USER_NOT_FOUND);
+            return "redirect:/login";
+        }
+
+        if (!foundUser.getPassword().equals(password)) {
+            redirectAttributes.addFlashAttribute("errorMessage", ErrorMessage.INVALID_PASSWORD);
+            return "redirect:/login";
+        }
+
+        sessionRepository.findByUserId(foundUser.getId()).ifPresent(sessionRepository::delete);
+
+        UserSession userSession = sessionRepository.save(sessionService.createSession(foundUser));
+        response.addCookie(new Cookie("sessionId", userSession.getId().toString()));
+
+        return "redirect:/weather";
     }
 }
--- src/app/src/main/java/org/weather/app/controller/RegisterController.java
+++ src/app/src/main/java/org/weather/app/controller/RegisterController.java
@@ -1,12 +1,48 @@
 package org.weather.app.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.weather.app.ErrorMessage;
+import org.weather.app.model.User;
+import org.weather.app.repository.UserRepository;
 
 @Controller
 public class RegisterController {
+    private final UserRepository userRepository;
+
+    public RegisterController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/register")
-    public String getRegister() {
+    public String get() {
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String post(RedirectAttributes redirectAttributes, HttpServletRequest request) {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        if (login.isBlank() || password.isBlank()) {
+            redirectAttributes.addFlashAttribute("errorMessage", ErrorMessage.INVALID_PARAMS);
+            return "redirect:/register";
+        }
+
+        if (userRepository.findByLogin(login).isPresent()) {
+            redirectAttributes.addFlashAttribute("errorMessage", ErrorMessage.USER_EXISTS);
+            return "redirect:/register";
+        }
+
+        User newUser = new User();
+        newUser.setLogin(login);
+        newUser.setPassword(password);
+
+        userRepository.save(newUser);
+
+        return "redirect:/login";
     }
 }
