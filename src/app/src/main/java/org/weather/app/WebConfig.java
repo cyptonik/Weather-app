eb5bf406fb9a85e652b0bd5b8eb747631d33225b
cyptonik <enixydex15@gmail.com>
2026-04-24 04:45:20 +0300

    updated webconfig cuz it couldn't let me display favicon

--- src/app/src/main/java/org/weather/app/WebConfig.java
+++ src/app/src/main/java/org/weather/app/WebConfig.java
@@ -3,16 +3,24 @@ package org.weather.app;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring6.SpringTemplateEngine; // или spring5
+import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @EnableWebMvc
+@EnableScheduling
 @Configuration
 @ComponentScan(basePackages = "org.weather.app")
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
