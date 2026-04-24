805bd26b9106ab482f1ee1e59bee59ce7355fec0
cyptonik <enixydex15@gmail.com>
2026-04-24 04:40:06 +0300

    updated WebAppInitializer

--- src/app/src/main/java/org/weather/app/WebAppInitializer.java
+++ src/app/src/main/java/org/weather/app/WebAppInitializer.java
@@ -3,9 +3,12 @@ package org.weather.app;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletRegistration;
 
+import jakarta.servlet.SessionTrackingMode;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import java.util.Collections;
 
 public class WebAppInitializer implements WebApplicationInitializer {
     @Override
@@ -14,9 +17,12 @@ public class WebAppInitializer implements WebApplicationInitializer {
                 new AnnotationConfigWebApplicationContext();
         ctx.register(WebConfig.class, HibernateConfig.class);
 
-        ServletRegistration.Dynamic appServlet = 
+        ServletRegistration.Dynamic appServlet =
 		        sc.addServlet("mvc", new DispatcherServlet(ctx));
         appServlet.setLoadOnStartup(1);
         appServlet.addMapping("/");
+
+        // нужно для передачи атрибутов
+        sc.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));
     }
 }
