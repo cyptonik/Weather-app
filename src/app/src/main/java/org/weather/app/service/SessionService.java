4ae0c4bc8331c6f1080a7e2793f1df91ec236d31
cyptonik <enixydex15@gmail.com>
2026-04-24 04:42:37 +0300

    added session validation and session extraction from cookies methods

--- src/app/src/main/java/org/weather/app/service/SessionService.java
+++ src/app/src/main/java/org/weather/app/service/SessionService.java
@@ -31,4 +31,33 @@ public class SessionService {
 
         return userSession;
     }
+
+    public UserSession getSessionFromCookie(Cookie[] cookies) {
+        if (cookies == null) {
+            return null;
+        }
+
+        UUID uuid = Arrays.stream(cookies)
+                .filter(c -> c.getName().equals("sessionId"))
+                .map(Cookie::getValue)
+                .findFirst()
+                .map(UUID::fromString)
+                .orElse(null);
+        if (uuid == null) {
+            return null;
+        }
+
+        return sessionRepository.findById(uuid).orElse(null);
+    }
+
+    public boolean isSessionValid(UserSession session) {
+        if (session == null) {
+            return false;
+        }
+        if (session.getExpires_at().isBefore(LocalDateTime.now())) {
+            sessionRepository.delete(session);
+            return false;
+        }
+        return true;
+    }
 }
