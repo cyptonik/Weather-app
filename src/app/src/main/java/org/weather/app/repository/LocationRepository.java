424ffd13dc2c8f6377ff2ccd0e3377adee9316b5
cyptonik <enixydex15@gmail.com>
2026-04-25 09:31:56 +0300

    created locations repository

---
+++ src/app/src/main/java/org/weather/app/repository/LocationRepository.java
@@ -0,0 +1,41 @@
+package org.weather.app.repository;
+
+import jakarta.transaction.Transactional;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+import org.weather.app.model.Location;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class LocationRepository {
+    private final SessionFactory sessionFactory;
+
+    public LocationRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void save(Location location) {
+        sessionFactory.getCurrentSession().persist(location);
+    }
+
+    public List<Location> findAllByUserId(Integer id) {
+        return sessionFactory.getCurrentSession()
+                .createQuery("FROM Location WHERE user.id = :id", Location.class)
+                .setParameter("id", id)
+                .list();
+    }
+
+    public Location findById(Integer id) {
+        return sessionFactory.getCurrentSession()
+                .createQuery("FROM Location WHERE id = :id", Location.class)
+                .setParameter("id", id)
+                .uniqueResult();
+    }
+
+    public void delete(Location location) {
+        sessionFactory.getCurrentSession().remove(location);
+    }
+}
