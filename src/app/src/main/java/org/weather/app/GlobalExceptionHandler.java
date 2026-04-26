9fd7f538d5bdd4b743ca95c08ed60390f5e24dc0
cyptonik <enixydex15@gmail.com>
2026-04-26 07:48:05 +0300

    dependency fix

--- src/app/src/main/java/org/weather/app/GlobalExceptionHandler.java
+++ src/app/src/main/java/org/weather/app/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.weather.app;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
