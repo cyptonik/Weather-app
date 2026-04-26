fd445ab68a0fc91eabe39cca31ffadc516871b41
cyptonik <enixydex15@gmail.com>
2026-04-26 08:47:42 +0300

    update: created new error messages

--- src/app/src/main/java/org/weather/app/ErrorMessage.java
+++ src/app/src/main/java/org/weather/app/ErrorMessage.java
@@ -11,4 +11,12 @@ public class ErrorMessage {
     public static final String INVALID_CITY = "Invalid city";
 
     public static final String TIMEOUT = "Timeout";
+
+    public static final String INVALID_LOGIN_LENGTH = "Login length must be between 4 and 30 symbols";
+    public static final String INVALID_LOGIN_SPECIAL_SYMBOLS = "Login can only contain numbers, letters, dashes and underscores";
+    public static final String INVALID_LOGIN_FIRST_SYMBOL = "Login must begin with a letter";
+
+    public static final String INVALID_PASSWORD_LENGTH = "Password length must be between 8 and 40 symbols";
+    public static final String INVALID_PASSWORD_SPECIAL = "Password must contain special symbols";
+    public static final String INVALID_PASSWORD_UPPERCASE = "Password must contain uppercase letters";
 }
