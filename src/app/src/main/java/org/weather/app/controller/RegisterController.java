d7db32d0ca38b154e3f96e57a669b4d384a5ed0a
cyptonik <enixydex15@gmail.com>
2026-04-28 03:28:15 +0300

    moved password encryption to a separate service class

--- src/app/src/main/java/org/weather/app/controller/RegisterController.java
+++ src/app/src/main/java/org/weather/app/controller/RegisterController.java
@@ -1,8 +1,6 @@
 package org.weather.app.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,22 +8,20 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.weather.app.ErrorMessage;
 import org.weather.app.model.User;
 import org.weather.app.repository.UserRepository;
+import org.weather.app.service.PasswordService;
 import org.weather.app.service.RegistrationService;
 
 @Controller
 public class RegisterController {
     private final RegistrationService registrationService;
-    @Value("${password.pepper}")
-    private String pepper;
-
-    @Value("${password.rounds}")
-    private int rounds;
 
     private final UserRepository userRepository;
+    private final PasswordService passwordService;
 
-    public RegisterController(UserRepository userRepository, RegistrationService registrationService) {
+    public RegisterController(UserRepository userRepository, RegistrationService registrationService, PasswordService passwordService) {
         this.userRepository = userRepository;
         this.registrationService = registrationService;
+        this.passwordService = passwordService;
     }
 
     @GetMapping("/register")
@@ -61,14 +57,10 @@ public class RegisterController {
 
         User newUser = new User();
         newUser.setLogin(login);
-        newUser.setPassword(hashPassword(password));
+        newUser.setPassword(passwordService.hashPassword(password));
 
         userRepository.save(newUser);
 
         return "redirect:/login";
-    }
-
-    private String hashPassword(String password) {
-        return BCrypt.hashpw(password + pepper, BCrypt.gensalt(rounds));
     }
 }
